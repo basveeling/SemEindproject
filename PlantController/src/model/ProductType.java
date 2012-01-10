05221fa4c5902202e45a4876957ef08fbd6e50a4
Patrick Bosch <patrickbosch92@gmail.com>
2012-01-10 12:09:29 +0100

    partbin-part & part-producttype

--- PlantController/src/model/ProductType.java
+++ PlantController/src/model/ProductType.java
@@ -3,12 +3,17 @@
  */
 package model;
 
+import java.util.ArrayList;
+
+import model.relations.ProductPart;
+
 /**
  * @author bas
  *
  */
 public class ProductType extends Part {
 	private int assemblyTime;
+	private ArrayList<ProductPart> madeFrom;
 
 	/**
 	 * @param inStock
@@ -16,6 +21,24 @@ public class ProductType extends Part {
 	public ProductType(int inStock, int assemblyTime) {
 		super(inStock);
 		this.assemblyTime = assemblyTime;
+	}
+
+	/**
+	 * @param e
+	 * @return
+	 * @see java.util.ArrayList#add(java.lang.Object)
+	 */
+	public boolean add(ProductPart e) {
+		return madeFrom.add(e);
+	}
+
+	/**
+	 * @param o
+	 * @return
+	 * @see java.util.ArrayList#remove(java.lang.Object)
+	 */
+	public boolean remove(Object o) {
+		return madeFrom.remove(o);
 	}
 
 	public int getAssemblyTime() {
